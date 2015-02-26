a719fb90be5636aaf01caefaa3d2b176a4a17228
Sergio <sergio@eclectico.net>
2015-02-26 16:16:07 +0100

    estructura de programa mapeada en un json

---
+++ programa-colaborativo_0.2/static/js/programa.js
@@ -0,0 +1,91 @@
+var listobjetivos={
+"objetivos":[
+    {"id":"madrid-democratico", 
+    	"titulo":"Apuesta por una ciudad habitable",
+    	"descripcion": "Ganemos Madrid defiende una ciudad habitable, equilibrada territorial y medioambientalmente, en la que cada barrio sea vivible, sostenible",
+    	 "apuesta":"ciudad-habitable"}, 
+    {"id":"eliminar-desigualdad",	
+     "titulo":"Eliminar la desigualdad y promover la cooperación entre los ámbitos territoriales",
+     "apuesta":"ciudad-habitable",
+     "descripcion":""},
+
+    {"id":"acceso-energia",	
+     "titulo":"Garantizar el acceso universal a la energía y al agua implementando una gestión basada en criterios sociales y medioambientales.",
+     "apuesta":"ciudad-habitable",
+     "descripcion":""},
+
+    {"id":"movilidad-eficiente",	
+     "titulo":"Establecer un modelo de movilidad eficiente y de bajo impacto ambiental.",
+     "apuesta":"ciudad-habitable",
+     "descripcion":""},
+
+    {"id":"tratamiento-residuos",	
+     "titulo":"Mejorar el tratamiento de residuos y vincularlo al ciclo ecológico y al desarrollo de espacios verdes y naturales.",
+     "apuesta":"ciudad-habitable",
+     "descripcion":""},
+
+    {"id":"descentralizar",	
+     "titulo":"Descentralizar y democratizar la organización del Ayuntamiento y promover la participación de la ciudadanía en la toma de decisiones",
+     "apuesta":"democracia-transparencia",
+     "descripcion":""},
+
+    {"id":"control-transparencia",	
+     "titulo":"",
+     "apuesta":"democracia-transparencia",
+     "descripcion":""},
+
+    {"id":"frenar-privatizacion",	
+     "titulo":"Frenar la privatizacion, el desmantelamiento de los servicios y la venta del patrimonio publico.",
+     "apuesta":"democracia-transparencia",
+     "descripcion":""},
+
+    {"id":"garantizar-fiscalizacion",   
+     "titulo":"Garantizar la fiscalización de la gestión económica y la determinación de las condiciones de la deuda a través de una auditoría ciudadana.",
+     "apuesta":"democracia-transparencia",
+     "descripcion":""},
+
+    {"id":"plan-autonomia",   
+     "titulo":"Diseñar un plan de autonomía y de gestión financiera eficiente, responsable y equitativa por parte del Ayuntamiento.",
+     "apuesta":"democracia-transparencia",
+     "descripcion":"democracia-transparencia"},
+
+    {"id":"acceso-vivienda",   
+     "titulo":"Garantizar el acceso a la vivienda en el ámbito municipal.",
+     "apuesta":"derechos-sociales",
+     "descripcion":""},
+
+    {"id":"acceso-universal",   
+     "titulo":"Garantizar el acceso universal a unos servicios públicos integrados con enfoque comunitario.",
+     "apuesta":"derechos-sociales",
+     "descripcion":""},
+
+    {"id":"diversidad-poblacion",   
+     "titulo":"Reconocer la diversidad de la poblacion de Madrid y garantizar su acceso a los recursos, infraestructuras y servicios publicos.",
+     "apuesta":"derechos-sociales",
+     "descripcion":""},
+
+    {"id":"acceso-cultura",   
+     "titulo":"Garantizar el libre acceso a la cultura y proteger el patrimonio histórico y la memoria cultural",
+     "apuesta":"derechos-sociales",
+     "descripcion":""},
+
+    {"id":"empleo-calidad",   
+     "titulo":"Garantizar la existencia de un modelo de empleo de calidad y en condiciones de equidad",
+     "apuesta":"modelo-sostenible",
+     "descripcion":""},
+
+     {"id":"trabajo-empresa",   
+     "titulo":"Apoyar al trabajo autonomo, a la pequeña empresa y al cooperativismo.",
+     "apuesta":"modelo-sostenible",
+     "descripcion":""},
+     
+    {"id":"sectores-estrategicos",   
+     "titulo":"Desarrollar sectores estratégicos implicados con la sostenibilidad medioambiental y el I+D+i.",
+     "apuesta":"modelo-sostenible",
+     "descripcion":""},
+     
+    {"id":"medidas-conciliacion",   
+     "titulo":"Implementar un sistema de medidas de conciliación y corresponsabilidad social",
+     "apuesta":"modelo-sostenible",
+     "descripcion":""}, 
+]}
