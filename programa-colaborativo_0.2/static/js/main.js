2b0eaed4ad5ef58bec7a64d26b7dd19e676c2f0e
Sergio <sergio@eclectico.net>
2015-03-02 03:46:49 +0100

    poniendo las nuevas categorias en formularios

--- programa-colaborativo_0.2/static/js/main.js
+++ programa-colaborativo_0.2/static/js/main.js
@@ -4,4 +4,10 @@ $(function(){
     $.each(listobjetivos.objetivos, function(index,jsonObject){	    
 	        $('#objetivos-'+jsonObject.apuesta).append('<a href="sector.php?sector='+jsonObject.apuesta+'"><div class="objetivo"> ' +jsonObject.titulo+'</div></a>');	   
 	});
+
+
+	/* sidebar*/
+	$.each(listobjetivos.objetivos, function(index,jsonObject){	    
+	        $('#enlaces-sidebar').append('<a href="sector.php?sector='+jsonObject.apuesta+'"><div class="objetivo"> ' +jsonObject.titulo+'</div></a>');	   
+	});
 });
