9e2817c7b34f4a8cf029fc2680dde51636279479
Sergio <sergio@eclectico.net>
2015-03-04 04:55:58 +0100

    fuentes para que no se queje y cambios js para dar vidilla

--- programa-colaborativo_0.2/static/js/main.js
+++ programa-colaborativo_0.2/static/js/main.js
@@ -2,7 +2,7 @@
 $(function(){
 
     $.each(listobjetivos.objetivos, function(index,jsonObject){	    
-	        $('#objetivos-'+jsonObject.apuesta).append('<a href="sector.php?sector='+jsonObject.apuesta+'"><div class="objetivo"> ' +jsonObject.titulo+'</div></a>');	   
+	        $('#objetivos-'+jsonObject.apuesta).append('<a href="sector.php?sector='+jsonObject.id+'"><div class="objetivo"> ' +jsonObject.titulo+'</div></a>');	   
 	});
 
 
@@ -10,4 +10,6 @@ $(function(){
 	$.each(listobjetivos.objetivos, function(index,jsonObject){	    
 	        $('#enlaces-sidebar').append('<a href="sector.php?sector='+jsonObject.apuesta+'"><div class="objetivo"> ' +jsonObject.titulo+'</div></a>');	   
 	});
+
+
 });
