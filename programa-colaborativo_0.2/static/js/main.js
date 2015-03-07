89a0dafe41c7b12112aa82a77bb5a47351ad932f
Sergio <sergio@eclectico.net>
2015-03-07 11:09:09 +0100

    mas mejoras disenio

--- programa-colaborativo_0.2/static/js/main.js
+++ programa-colaborativo_0.2/static/js/main.js
@@ -10,6 +10,9 @@ $(function(){
 	$.each(listobjetivos.objetivos, function(index,jsonObject){	    
 	        $('#enlaces-sidebar').append('<a href="sector.php?sector='+jsonObject.apuesta+'"><div class="objetivo"> ' +jsonObject.titulo+'</div></a>');	   
 	});
-
-
+	if (typeof distritos != 'undefined') {
+		$.each(distritos, function(index,jsonObject){	    
+		        $('#disabledSelect').append('<option value="'+index+'">'+ jsonObject+ '</option>');	   
+		});
+	}
 });
