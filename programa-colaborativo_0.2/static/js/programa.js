2b0ff042cdcdc7b86388583c42e4b7a2f9ff91ea
Sergio <sergio@eclectico.net>
2015-03-12 04:08:23 +0100

    bugs IE9 y descripcion programa

--- programa-colaborativo_0.2/static/js/programa.js
+++ programa-colaborativo_0.2/static/js/programa.js
@@ -5,90 +5,90 @@ var listobjetivos={
     {"id":"vivienda-digna",   
      "titulo":"Garantizar el acceso a una vivienda digna ",
      "apuesta":"derechos-equidad",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
    {"id":"servicios-publicos",   
      "titulo":"Garantizar el acceso universal a los servicios públicos  ",
      "apuesta":"derechos-equidad",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
    {"id":"vida-barrios",   
      "titulo":"Potenciar la vida en los barrios ",
      "apuesta":"derechos-equidad",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
    {"id":"calidad-vida",   
      "titulo":"Mejorar la calidad de vida reconociendo la diversidad",
      "apuesta":"derechos-equidad",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
 
 
 //ciudad sostenible
     {"id":"interes-general", 
     	"titulo":"Realizar un planeamiento basado en el interés general",
-    	"descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum</p>.",
+    	"descripcion":"<p>Reforma estructural del planeamiento ciudadano que parta de procesos de construcción colectiva de planes locales y su integración en un planteamiento metropolitano al servicio de los ciudadanos. AHORA MADRID Pondrá en marcha medidas urgentes que frenen los procesos especulativos en marcha</p>.",
         "apuesta":"ciudad-sostenible"}, 
 
     {"id":"espacio-publico-patrimonio",	
      "titulo":"Recuperar el espacio público y el patrimonio",
      "apuesta":"ciudad-sostenible",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":"<p> Recuperar un Madrid Saludable que ponga en valor y en uso su patrimonio histórico, artístico, inmaterial, de propiedades, inmuebles, saberes,... AHORA MADRID emprenderá acciones que palíen las situaciones urbanas de mayor emergencia.</p>"},
 
     {"id":"ecologia-urbana",	
      "titulo":"Establecer políticas de ecología urbana",
      "apuesta":"ciudad-sostenible",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":"<p>La mejora de la calidad del aire, un acceso al agua público y racional y un mayor ahorro energético son medidas que nos convienen a los madrileños, así como una mirada atenta a las necesidades del planeta. AHORA MADRID emprenderá acciones encaminadas al ahorro energético, la mejor gestión de residuos y un modelo urbano ecológico.</p>"},
 
     {"id":"movilidad",	
      "titulo":"Desarrollar una movilidad eficiente y sostenible",
      "apuesta":"ciudad-sostenible",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":" <p>La movilidad de los madrileños necesita colaborar con la mejor calidad del aire y la mejor calidad del espacio urbano, optimizando recursos para favorecer mejores desplazamientos a todas las personas. AHORA MADRID pondrá en marcha medidas para conciliar las necesidades de desplazamiento de los madrileños con el medio ambiente y la calidad del espacio urbano.</p>"},
 
     {"id":"cohesion-territorial",    
      "titulo":"Promover la cohesión territorial",
      "apuesta":"ciudad-sostenible",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":"<p>Los barrios y distritos que componen la ciudad de Madrid presentan un alto desequilibrio socioeconómico. AHORA MADRID pondrá en marcha herramientas de planeamiento y gestión urbana que faciliten la relación urbana y política entre los territorios que componen Madrid.  </p>"},
 
 //gobierno democratico
     {"id":"auditar-deuda",	
      "titulo":"Auditar la deuda y la gestión municipal",
      "apuesta":"gobierno-democratico",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
 
     {"id":"autonomia-economica",	
      "titulo":"Desarrollar la autonomía económica y una gestión equitativa",
      "apuesta":"gobierno-democratico",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
 
     {"id":"reforma-administracion",    
      "titulo":"Reformar la administración local",
      "apuesta":"gobierno-democratico",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
 
 
     {"id":"participacion-ciudadana",	
      "titulo":"Establecer una mayor participación ciudadana",
      "apuesta":"gobierno-democratico",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. <p>Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
 
     {"id":"descentralizar-equilibrio",    
      "titulo":"Descentralizar y favorecer el equilibrio territorial",
      "apuesta":"gobierno-democratico",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. <p>Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
 
 // economia-empleo
 
     {"id":"generar-empleo",   
      "titulo":"Generar empleo estable, digno y equitativo",
      "apuesta":"economia-empleo",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
 
     {"id":"pymes-economiasocial",   
      "titulo":"Apoyar las PYMES, el trabajo autónomo y la economía social",
      "apuesta":"economia-empleo",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
 
     {"id":"cambio-modelo",   
      "titulo":"Cambio de modelo económico",
      "apuesta":"economia-empleo",
-     "descripcion":" Descripcion del objetivo. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, <p>cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>"},
+     "descripcion":""},
     {"id":"sin-categoria",   
      "titulo":"Propuestas sin clasificar",
      "apuesta":"",
@@ -105,7 +105,8 @@ var apuestas={
     },
     'ciudad-sostenible':{
         "titulo": "Ciudad sostenible, cohesionada y cercana",
-        "descripcion": "Descripcion de la apuesta. Lorem Ipsum es simplemente el texto de rellen",
+        "descripcion": "AHORA MADRID apuesta por la defensa de una modelo de ciudad diseñado y gobernado por sus vecinas y vecinos. \
+        El modelo de gestión de la ciudad de Madrid viene estando al servicio de los intereses de unos pocos. Proponemos como objetivos la puesta en marcha de un nuevo planeamiento al servicio de los ciudadanos, la regeneración de la forma de uso y el cuidado del espacio urbano y el patrimonio común. Una rehabilitación física y social de la ciudad que requiere de medidas urgentes y un proceso profundo de participación para definir estrategias eficaces que cubran los intereses de todas las personas.",
     },
     'gobierno-democratico':{
         "titulo": "Gobierno Democrático, transparente y eficaz",
