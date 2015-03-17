a5719ee5b30281734b1a7d08638cfb3d583c9fa4
Sergio <sergio@eclectico.net>
2015-03-17 02:50:30 +0100

    mejoras subir votos y comentar menos

--- programa-colaborativo_0.2/index.php
+++ programa-colaborativo_0.2/index.php
@@ -21,11 +21,20 @@ FROM users AS u, prog_propuestas AS p
 WHERE  `autor_id` = u.id
 ORDER BY p.id DESC LIMIT 12; ';
 
-$consensuadas = 
+/*$consensuadas = 
 'SELECT u.nombre, u.apellidos, p.id, p.titulo, p.comentarios, p.sum_likes, p.sector, p.barrio, u.id_rol, p.positivos, p.negativos, (LOG(p.positivos+p.negativos)* ((p.positivos-p.negativos) /(p.positivos+p.negativos))) log, (p.positivos /(p.positivos+p.negativos)) porcentaje
 FROM users AS u, prog_propuestas AS p
 WHERE  `autor_id` = u.id
-ORDER BY log DESC LIMIT 12; ';
+ORDER BY log DESC LIMIT 12; ';*/
 
-$datos = array('user'=>autentificado(),'propuestas'=>listar($consulta), 'debatidas'=>listar($debatidas), 'recientes'=>listar($recientes), 'consensuadas'=>listar($consensuadas));
+$consensuadas = 'SELECT u.nombre, u.apellidos, u.id_rol, p.id, p.titulo, p.comentarios, p.sum_likes, p.positivos, p.negativos, p.sector, p.barrio
+FROM users AS u, prog_propuestas AS p
+WHERE  `autor_id` = u.id AND p.sum_likes BETWEEN 8 AND 20
+ORDER BY p.id DESC LIMIT 12; ';
+
+$total = 'SELECT count(id) AS total FROM prog_propuestas';
+$numeroPropuestas = listar($total);
+
+
+$datos = array('user'=>autentificado(),'propuestas'=>listar($consulta), 'debatidas'=>listar($debatidas), 'recientes'=>listar($recientes), 'consensuadas'=>listar($consensuadas),'totalPropuestas' =>$numeroPropuestas['0']['total']);
 echo $template->render($datos);
--- programa-colaborativo_0.2/todas.php
+++ programa-colaborativo_0.2/todas.php
@@ -31,11 +31,16 @@ FROM users AS u, prog_propuestas AS p
 WHERE  `autor_id` = u.id
 ORDER BY p.id DESC LIMIT '. $empezarPor .', '. $resultadosPorPagina .'; ';
 
-$consensuadas = 
+/*$consensuadas = 
 'SELECT u.nombre, u.apellidos, p.id, p.titulo, p.comentarios, p.sum_likes, p.sector, p.barrio, p.positivos, p.negativos, u.id_rol, (LOG(p.positivos+p.negativos)* ((p.positivos-p.negativos) /(p.positivos+p.negativos))) log, (p.positivos /(p.positivos+p.negativos)) porcentaje
 FROM users AS u, prog_propuestas AS p
 WHERE  `autor_id` = u.id
-ORDER BY log DESC LIMIT '. $empezarPor .', '. $resultadosPorPagina .'; ';
+ORDER BY log DESC LIMIT '. $empezarPor .', '. $resultadosPorPagina .'; ';*/
+
+$consensuadas = 'SELECT u.nombre, u.apellidos, u.id_rol, p.id, p.titulo, p.comentarios, p.sum_likes, p.positivos, p.negativos, p.sector, p.barrio
+FROM users AS u, prog_propuestas AS p
+WHERE  `autor_id` = u.id AND p.sum_likes BETWEEN 8 AND 20
+ORDER BY p.id DESC LIMIT '. $empezarPor .', '. $resultadosPorPagina .'; ';
 	
 	//Paginación
 	$numeroPropuestas = listar($total);
@@ -50,5 +55,5 @@ ORDER BY log DESC LIMIT '. $empezarPor .', '. $resultadosPorPagina .'; ';
 	
 	//echo print_r($numeroPropuestas) . "Todas: ".$numeroPropuestas['0']['total'];
 
-$datos = array('user'=>autentificado(),'propuestas'=>listar($consulta), 'debatidas'=>listar($debatidas), 'recientes'=>listar($recientes), 'consensuadas'=>listar($consensuadas), 'paginado'=>$paginado);
+$datos = array('user'=>autentificado(),'propuestas'=>listar($consulta), 'debatidas'=>listar($debatidas), 'recientes'=>listar($recientes), 'consensuadas'=>listar($consensuadas), 'paginado'=>$paginado, 'totalPropuestas' =>$numeroPropuestas['0']['total']);
 echo $template->render($datos);
